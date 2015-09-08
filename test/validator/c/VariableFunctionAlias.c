cf7ff247610ea2d422b0a47aff0c718c7ef11ece
Tarun Prabhu <tprabhu2@illinois.edu>
2015-09-08 11:47:41 -0500

    Fixed the tests to either explicitly fail or to support the new metadata syntax in LLVM 3.6

--- test/validator/c/VariableFunctionAlias.c
+++ test/validator/c/VariableFunctionAlias.c
@@ -1,5 +1,5 @@
 // RUN: %dragonegg -S %s -o - | FileCheck %s
-// XFAIL: gcc-4.8
+// XFAIL: gcc-4.8, gcc-4.9, gcc-5.0, gcc-5.1, gcc-5.2, gcc-5.3, gcc-5.4
 
 void qux(void) { };
 extern int foo __attribute__ ((alias ("qux")));
